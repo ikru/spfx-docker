21920ff5d534a7e4515e7dadd1d468dca1068587
ivan <ikru@inbox.ru>
2021-05-06 02:01:01 -0400

    Adds one more ln and mkdir

--- scripts/spfx-gulp-serve.sh
+++ scripts/spfx-gulp-serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose exec "spfx-debug" bash -c "ln -sf -T /npm/node_modules /app/spfx/node_modules && gulp serve --nobrowser"
+docker-compose exec "spfx-debug" bash -c "ln -sf -T /npm/node_modules /app/spfx/node_modules && mkdir -p /app/spfx/npm && ln -sf -T /npm/node_modules /app/spfx/npm/node_modules && gulp serve --nobrowser"
